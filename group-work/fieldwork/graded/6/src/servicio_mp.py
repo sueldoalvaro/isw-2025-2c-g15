2708d00298612134c598c8663542c07fc71bc599
pedroberecibar <pedroberecibar.dev@gmail.com>
2025-10-09 19:58:41 -0300

    refactor: Mejora de codigo y de pruebas de compra con tarjeta de credito

--- group-work/fieldwork/graded/6/src/servicio_mp.py
+++ group-work/fieldwork/graded/6/src/servicio_mp.py
@@ -1,5 +1,7 @@
 def procesar_pago_mp(monto_total, datos_tarjeta):
+    print(f"Conectando con MercadoPago para procesar un pago de {monto_total} con los datos de la tarjeta: {datos_tarjeta}")
     pass
 
 def enviar_mail_confirmacion(email_cliente, detalles_compra):
+    print(f"Enviando correo de confirmación a {email_cliente} con los detalles: {detalles_compra}")
     pass
